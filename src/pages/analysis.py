a3417742be304eb7447fa8286840b7231253fd5c
limkeunhyeok <gorloom6425@naver.com>
2024-06-10 15:39:53 +0900

    fix: clean up code

--- src/pages/analysis.py
+++ src/pages/analysis.py
@@ -13,31 +13,40 @@ def find_market(markets, code):
             return market
 
 def write():
-    markets = get_markets()
-
-    enabled_markets = [market for market in markets if market['isEnabled']]
-
-    market_codes = [f'{market["code"]}' for market in enabled_markets]
-    code = st.selectbox(
-        "market??",
-        market_codes,
-        key='selected_market_code',
-    )
-
-    target_market = find_market(enabled_markets, code)
-
-    now = get_now()
-    start_date = sub_days(now, 5)
-    end_date = add_days(now, 1)
-
-    data = get_coins(code, start_date, end_date, 100)
-
-    df = pd.DataFrame(data['data'])
-
-    df['MA20'] = df["tradePrice"].rolling(window=20).mean()
-    df['MA5'] = df["tradePrice"].rolling(window=5).mean()
-
-
     with st.spinner("Loading Home ..."):
+        # title
+        st.write(
+            """
+            # Analysis
+            """
+        )
+
+        # body
+        # selectbox
+        markets = get_markets()
+
+        enabled_markets = [market for market in markets if market['isEnabled']]
+
+        market_codes = [f'{market["code"]}' for market in enabled_markets]
+        code = st.selectbox(
+            "Choose a market",
+            market_codes,
+            key='selected_market_code',
+        )
+
+        target_market = find_market(enabled_markets, code)
+
+        now = get_now()
+        start_date = sub_days(now, 5)
+        end_date = add_days(now, 1)
+
+        data = get_coins(code, start_date, end_date, 100)
+
+        df = pd.DataFrame(data['data'])
+
+        df['MA20'] = df["tradePrice"].rolling(window=20).mean()
+        df['MA5'] = df["tradePrice"].rolling(window=5).mean()
+
+        # metric and chart
         write_metric(target_market, df)
         write_candlestick(df)
--- src/pages/dataset.py
+++ src/pages/dataset.py
@@ -6,29 +6,42 @@ from src.components.candlestick import write_candlestick
 
 def write():
     with st.spinner("Loading Home ..."):
+        # title
         st.write(
             """
-## The Magic of Streamlit
-
-The only way to truly understand how magical Streamlit is to play around with it.
-But if you need to be convinced first, then here is the **4 minute introduction** to Streamlit!
-
-Afterwards you can explore examples in the Gallery and go to the [Streamlit docs](https://streamlit.io/docs/) to get started.
-    """
+            # Dataset
+            """
         )
+
+        # body
+        # info
+        st.write(
+            """
+            What is Lorem Ipsum?
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+            """
+        )
+
+        # file upload
         file = st.file_uploader("Choose a file")
         if file is None:
             return
+
+
+        if file.type == 'text/csv':
+            df = pd.read_csv(file)
+
+        if file.type == 'application/json':
+            df = pd.read_json(file)
 
         if file.type != 'text/csv' and file.type != 'application/json':
             st.write('Please json or csv!')
             return
 
-        df = pd.read_json(file)
         st.write(df)
-        st.write(df.keys())
 
-        st.write("select market and time field!")
+        # select field
+        st.write("Choose a market and time field")
         market_column, time_column = st.columns([1, 1])
 
         selected_market_field = market_column.selectbox('Market Field', df.keys(), index=None)
@@ -37,7 +50,7 @@ Afterwards you can explore examples in the Gallery and go to the [Streamlit docs
         if selected_market_field is None or selected_time_field is None:
             return
 
-        st.write("select ohlc field!")
+        st.write("Choose a ohlc field")
         open_column, high_column, low_column, close_column = st.columns([1, 1, 1, 1])
 
         selected_open_field = open_column.selectbox('Open Price Field', df.keys(), index=None)
@@ -53,7 +66,7 @@ Afterwards you can explore examples in the Gallery and go to the [Streamlit docs
         ):
             return
 
-        st.write("select accumulated field!")
+        st.write("Choose a accumulated field")
         acc_price_column, acc_volumn_column = st.columns([1, 1])
 
         selected_acc_price_field = acc_price_column.selectbox('Acc Price Field', df.keys(), index=None)
@@ -78,5 +91,6 @@ Afterwards you can explore examples in the Gallery and go to the [Streamlit docs
         converted_df['MA20'] = converted_df["tradePrice"].rolling(window=20).mean()
         converted_df['MA5'] = converted_df["tradePrice"].rolling(window=5).mean()
 
+        # chart
         write_candlestick(converted_df)
 
